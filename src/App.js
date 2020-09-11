b76c42ef3ea9253b04b629f28b8a2e9a3e83199d
marioleonel <mario.ab.leonel@gmail.com>
2020-09-11 08:35:27 -0300

    troca de texto

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edite <code>src/App.js</code> e salve para reload.
+          Edite <code>src/App.js</code> e salve para feature branch.
         </p>
         <a
           className="App-link"
